04d7a74beac12296298ee981b2534e6fde3ae1d7
Nikita Latushka <151414800+nikitalatushka@users.noreply.github.com>
2024-08-12 17:14:21 -0400

    set up swagger documentation at /api-docs

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const app = express();
 const port = 3000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
