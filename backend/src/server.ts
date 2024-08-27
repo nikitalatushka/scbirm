e53ec3ac19c523572e479ab5f89b2e0bd5eedfca
Nikita Latushka <151414800+nikitalatushka@users.noreply.github.com>
2024-08-27 00:36:04 -0400

    express app listening

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,2 +1,14 @@
 console.log("'Hello world' from ./src/server");
 
+import express from "express";
+
+const app = express ();
+const port = 9090;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port} from ./src/server`)
+});
